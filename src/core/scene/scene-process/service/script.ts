3505276e6c2c2193a93c506ac73d42726ef44c25
fengyuzhe <song2008_2001@126.com>
2026-05-26 14:27:05 +0800

    refine

--- src/core/scene/scene-process/service/script.ts
+++ src/core/scene/scene-process/service/script.ts
@@ -50,42 +50,7 @@ class AsyncIterationConcurrency1 {
 const importExceptionLogTag = '::SceneExecutorImportExceptionHandler::';
 const importExceptionLogRegex = new RegExp(importExceptionLogTag);
 
-class GlobalEnv {
-    public async record(fn: () => Promise<void>) {
-        this.clear();
-        this._queue.push(async () => {
-            const beforeKeys = Object.keys(globalThis);
-            await fn();
-            const afterKeys = Object.keys(globalThis);
-            for (const afterKey of afterKeys) {
-                if (!beforeKeys.includes(afterKey)) {
-                    this._incrementalKeys.add(afterKey);
-                }
-            }
-            console.debug(`Incremental keys: ${Array.from(this._incrementalKeys)}`);
-        });
-        await this.processQueue(); // 处理队列
-    }
-
-    private clear() {
-        this._queue.push(async () => {
-            for (const incrementalKey of this._incrementalKeys) {
-                delete (globalThis as any)[incrementalKey];
-            }
-            this._incrementalKeys.clear();
-        });
-    }
-
-    private async processQueue() {
-        while (this._queue.length > 0) {
-            const next = this._queue.shift();
-            if (next) await next(); // 执行队列中的下一个任务
-        }
-    }
-
-    private _incrementalKeys = new Set<string>();
-    private _queue: (() => Promise<void>)[] = [];
-}
+import { GlobalEnv } from '../../common/global-env';
 
 const globalEnv = new GlobalEnv();
 
